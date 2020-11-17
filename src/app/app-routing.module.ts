77c531cbde8518fb1f7422caac4fde57fc537b61
Gaurav Mendse <gaurav.mendse@icloud.com>
2020-11-17 11:45:58 +0530

    lazy loading modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,22 @@ const routes: Routes = [
   /** Empty path is route of every path,
    * only redirect when the full path is empty by adding pathMatch */
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  {
+    path: 'recipes',
+    loadChildren: () =>
+      import('./recipes/recipes.module').then((m) => m.RecipesModule),
+  },
+  {
+    path: 'shopping-list',
+    loadChildren: () =>
+      import('./shopping-list/shopping-list.module').then(
+        (m) => m.ShoppingListModule
+      ),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,23 +14,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 /** Custom Modules */
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
+  /** Do NOT import lazily loaded feature modules here, only eagerly loading modules here */
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
     CoreModule,
-    RecipesModule,
-    ShoppingListModule,
-    AuthModule,
   ],
 
   bootstrap: [AppComponent],
