28448f0e029ba9b8447a7f3ed107b43ba3d2ed69
Gaurav Mendse <gaurav.mendse@icloud.com>
2020-11-17 11:14:46 +0530

    core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -9,11 +9,6 @@ import { AppRoutingModule } from './app-routing.module';
 // import { BasicHighlightDirective } from './utils/basic-highlight.directive'; //Attribute
 // import { BetterHighlightDirective } from './utils/better-hightlight.directive'; //Attribute
 // import { UnlessDirective } from './utils/unless.directive'; //Structural
-
-/** Custom Services */
-import { RecipeService } from './recipes/recipe.service';
-import { ShoppingService } from './shopping-list/shopping-list.service';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 /** Custom Components */
 import { AppComponent } from './app.component';
@@ -24,6 +19,7 @@ import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
@@ -43,16 +39,9 @@ import { SharedModule } from './shared/shared.module';
     RecipesModule,
     ShoppingListModule,
     SharedModule,
+    CoreModule,
   ],
-  providers: [
-    RecipeService,
-    ShoppingService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
-  ],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/core.module.ts
@@ -0,0 +1,22 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+
+/** Custom Feature services (better approach, use {providedIn: 'root'} for these) */
+import { RecipeService } from './recipes/recipe.service';
+import { ShoppingService } from './shopping-list/shopping-list.service';
+
+@NgModule({
+  /** Services are not required to be exported (like components), since they are automatically injected in the root module */
+  providers: [
+    RecipeService,
+    ShoppingService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
+})
+export class CoreModule {}
